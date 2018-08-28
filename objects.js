4b37f4018af393d3dab5304b79a2d40183264090
Samuel Prada <jspradac@gmail.com>
2018-08-28 12:11:52 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {keys: "0"};
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song;
+  return playlist;
+}
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist;
+  return playlist;
+}
